1868ede428ee870a4938340f11731e88945b5235
Pellegrino Prevete <dvorak@0x87003Bd6C074C713783df04f36517451fF34CBEf>
2026-06-26 19:33:07 +0000

    HURR

--- evm-contract-deployment-versions.webpack.config.cjs
+++ evm-contract-deployment-versions.webpack.config.cjs
@@ -1,25 +1,29 @@
-/**    ----------------------------------------------------------------
- *     evm-contract-deployments-versions.webpack.config.cjs
- *     ----------------------------------------------------------------
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+/**    ---------------------------------------------------------
+ *     evm-contract-deployment-versions.webpack.config.cjs
+ *     ---------------------------------------------------------
  *     Copyright ©
  *       Pellegrino Prevete
  *         2024, 2025, 2026
  * 
  *     All rights reserved
- *     ----------------------------------------------------------------
+ *     ---------------------------------------------------------
  * 
  *     This program is free software: you can redistribute it and/or
- *     modify it under the terms of the GNU General Public License as
- *     published by the Free Software Foundation, either version 3 of
- *     the License, or (at your option) any later version.
+ *     modify it under the terms of the GNU General Public License
+ *     as published by the Free Software Foundation, either
+ *     version 3 of the License, or (at your option) any later
+ *     version.
  * 
- *     This program is distributed in the hope that it will be useful,
- *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *     GNU General Public License for more details.
+ *     This program is distributed in the hope that it will be
+ *     useful, but WITHOUT ANY WARRANTY; without even the implied
+ *     warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ *     PURPOSE.
+ *     See the GNU General Public License for more details.
  * 
- *     You should have received a copy of the GNU General Public License
- *     along with this program.
+ *     You should have received a copy of the
+ *     GNU General Public License along with this program.
  *     If not, see <https://www.gnu.org/licenses/>.
  */
 
@@ -63,6 +67,10 @@ const
   _ignore_plugin =
     _webpack.IgnorePlugin; 
 const
+  _node_fs_ignore_plugin =
+    new _ignore_plugin(
+          _node_fs_ignore);
+const
   _yargs_ignore_plugin =
     new _ignore_plugin(
           _yargs_ignore);
@@ -80,6 +88,10 @@ module.exports = {
   },
   resolve: {
     alias: {
+      "node:fs":
+        _path.resolve(
+          __dirname,
+          'node_modules/opfs/opfs'),
       "fs":
         _path.resolve(
           __dirname,
@@ -87,11 +99,11 @@ module.exports = {
       "evm-contracts-tools":
         _path.resolve(
           __dirname,
-          "libevm-contract-tools"),
+          "evm-contracts-tools"),
       "evm-transactions-tools":
         _path.resolve(
           __dirname,
-          "node_modules/evm-transactions-tools/libevm-transactions-tools"),
+          "node_modules/evm-transactions-tools/evm-transactions-tools"),
       "evm-wallet.js":
         _path.resolve(
           __dirname,
@@ -136,6 +148,7 @@ module.exports = {
     { yargs:
         'yargs' },
   plugins: [
+    _node_fs_ignore_plugin,
     _yargs_ignore_plugin,
     _yargs_helpers_ignore_plugin
   ]
